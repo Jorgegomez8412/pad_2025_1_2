0f16273d834119ff6a0e3c992b099113b9c8448f
Jandres1112 <105859049+Jandres1112@users.noreply.github.com>
2025-05-11 07:14:10 -0500

    Empaquetado de dependencias

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+   name="edu_pad",
+   version="0.0.1",
+   author="Jorge Gòmez",
+   author_email="jorge.gomez65@est.iudigital.edu.co",
+   description="scraping para extraer informaciòn de paginas para su analisis posterior para la materia programaciòn de analisis de datos ",
+   py_modules=[" "],
+   install_requires=[
+   "pandas",
+   "openpyxl",
+   "requests",
+   "beautifulsoup"
+   ]
+)
